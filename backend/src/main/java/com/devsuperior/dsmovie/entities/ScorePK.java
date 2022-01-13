bd387492a6e5a72d52f82dcdc56f246898ca749b
Thomaz Braga <thomazmarciano@gmail.com>
2022-01-13 12:37:50 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/ScorePK.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsmovie.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Embeddable
+public class ScorePK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
